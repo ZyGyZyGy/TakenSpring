026a5351db622103b6d4b78702b72f6d41608e82
Aleks <Aleks@LOK111-05>
2017-04-25 14:48:19 +0200

    1.2 AantalKinderenTonen

--- src/main/java/be/vdab/presentation/PersoonViewer.java
+++ src/main/java/be/vdab/presentation/PersoonViewer.java
@@ -6,10 +6,24 @@ import be.vdab.entities.Persoon;
 
 public class PersoonViewer {
 
+    private boolean aantalKinderenTonen;
+
+    public boolean isAantalKinderenTonen() {
+	return aantalKinderenTonen;
+    }
+
+    public void setAantalKinderenTonen(boolean aantalKinderenTonen) {
+	this.aantalKinderenTonen = aantalKinderenTonen;
+    }
+
     public void afbeelden(List<Persoon> list) {
 	for (Persoon persoon : list) {
-	    System.out.println(persoon.toString());
+	    System.out.println(
+		    persoon.getPersoonNr()
+		    + "; " + persoon.getVoornaam()
+		    + "; " + persoon.getFamilienaam()
+		    + "; " + (isAantalKinderenTonen() ? persoon.getAantalKinderen() : ""));
 	}
     }
-    
+
 }
